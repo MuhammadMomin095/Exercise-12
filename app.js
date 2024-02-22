fe390bdee8a6d68c71a57f1ead2c9f92d0fab840
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-02-22 14:45:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var members = ['Umer', 'Meer', 'Waleed', 'Hassan', 'Multazim'];
+var message = 'kia haal ha sab ka:';
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let members: string[] =['Umer','Meer','Waleed','Hassan','Multazim'];
+let message: string='kia haal ha sab ka:';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
